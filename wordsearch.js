5a026b853258d63649356a39bca0ad4474045c51
Rene Wang <rene.wang1@hotmail.com>
2020-12-02 21:31:14 +0000

    working function including backwards and vertical backwards

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const horizontalReverse = horizontalJoin;
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let l of horizontalReverse) {
+    let m = l.split('').reverse().join('');
+    if (m.includes(word)) return true;
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  const verticalReverse = verticalJoin;
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let l of verticalReverse) {
+    let m = l.split('').reverse().join('');
+    if (m.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let newArr = [];
+  
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArr.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      newArr[i].push(matrix[j][i]);
+    }
+  }
+  return newArr;
+};
+
+module.exports = wordSearch;
